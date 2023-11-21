8f0afd9fad9453351d536a46c48794aa2835824a
Nataliia <dominok56@gmail.com>
2023-11-21 22:06:33 +0200

    added urls for borrowing

--- backend/borrowings/urls.py
+++ backend/borrowings/urls.py
@@ -1,1 +1,14 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from borrowings.views import BorrowingViewSet
+
+
+router = routers.DefaultRouter()
+router.register("", BorrowingViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "borrowings"
