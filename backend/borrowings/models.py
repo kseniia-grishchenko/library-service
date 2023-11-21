fd8d71ee359f7156344a8ad8073e71ca2eeb3bbd
Nataliia <dominok56@gmail.com>
2023-11-21 21:51:54 +0200

    added borrowing model

--- backend/borrowings/models.py
+++ backend/borrowings/models.py
@@ -3,15 +3,22 @@ from django.db import models
 from django.db.models import Q, F
 
 from books.models import Book
+from .utils import calculate_expected_return_date
 
 
 class Borrowing(models.Model):
+    BORROW_TERM = 21
+    EXPIRED_DATA_PRICE_FACTOR = 1.1
+
     borrow_date = models.DateField(auto_now_add=True)
-    expected_return_date = models.DateField()
+    expected_return_date = models.DateField(
+        null=True,
+        default=calculate_expected_return_date(borrow_days=BORROW_TERM),
+    )
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="borrowings")
     is_active = models.BooleanField(default=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
 
     class Meta:
         constraints = [
@@ -38,7 +45,7 @@ class Borrowing(models.Model):
     @property
     def fine_price(self):
         return (
-                self.book.daily_annual_fee
+                self.book.daily_annual_fee or self.BORROW_TERM
                 * (self.actual_return_date - self.expected_return_date).days
         ) if self.actual_return_date else None
 
