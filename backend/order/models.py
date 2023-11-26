302894a121e308a8aa197a779324f87c18ed99e7
Nataliia <dominok56@gmail.com>
2023-11-26 22:28:37 +0200

    added borrowing field

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from borrowings.models import Borrowing
+
 
 class Order(models.Model):
     username = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     phone = models.CharField(max_length=13)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.username} {self.phone}"
